72b4cb852ff68412faf3da025c4005dfb5c8c3a7
DAXILSAXENA <daxildaxil@gmail.com>
2021-05-31 23:11:54 +0530

    Added store in dictionary and calculate singlet percentage

--- flipCoinCombination.sh
+++ flipCoinCombination.sh
@@ -4,60 +4,104 @@ Head=0
 Tail=1
 count=0
 sum=0
-HH=0
-HT=0
-TH=0
-TT=0
+HHH=0
+HHT=0
+HTH=0
+HTT=0
+THH=0
+THT=0
+TTH=0
+TTT=0
 
-read -p "Enter how many times you want to flip the coin twice :" number
+read -p "Enter how many times you want to flip the coin thrice :" number
 
 while [ $count -lt $number ]
+
 do
         a=$(( RANDOM % 2 ))
 
-        if [ $a -eq 0 ]
-        then
+	        if [ $a -eq 0 ]
+        	then
 
-	b=$(( RANDOM % 2 ))
+		b=$(( RANDOM % 2 ))
 	
-	if [ $b -eq 0 ]
-	then
-        Coin[((sum))]='HH'
-	(( HH++ ))
-	else
-	Coin[((sum))]='HT'
-	(( HT++ ))
-	fi
+			if [ $b -eq 0 ]
+			then
 
-        else
-	c=$(( RANDOM % 2 ))
+			d=$(( RANDOM % 2 ))
+	
+				if [ $d -eq 0 ]
+				then
+        			Coin[((sum))]='HHH'
+				(( HHH++ ))
+				else
+				Coin[((sum))]='HHT'
+				(( HHT++ ))
+				fi
+	
+			else
+	
+				if [ $d -eq 0 ]
+				then
+				Coin[((sum))]='HTH'
+				(( HTH++ ))
+				else
+				Coin[((sum))]='HTT'
+				(( HTT++ ))
+				fi
+			fi
 
-	if [ $b -eq 0 ]
-	then
-	Coin[((sum))]='TH'
-	(( TH++ ))
-	else
-	Coin[((sum))]='TT'
-        (( TT++ ))
-        fi
+		else
+	
+			if [ $b -eq 1 ]
+			then
+				if [ $d -eq 1 ]
+				then
+				Coin[((sum))]='TTT'
+				(( TTT++ ))
+				else
+				Coin[((sum))]='TTH'
+				(( TTH++ ))
+				fi
 
-        fi
+        		else
+				if [ $d -eq 0 ]
+				then
+				Coin[((sum))]='THH'
+				(( THH++ ))
+				else
+				Coin[((sum))]='THT'
+				(( THT++ ))
+				fi
+			fi
+		fi
+(( sum++ ))
+(( count++ ))
 
-	(( sum ++ ))
-        (( count++ ))
 done
 
 echo ${Coin[*]}
 
-HHpercent=$( echo $HH $number | awk '{print $1/$2*100}' )
-echo "Percent of Doublet combination(HH) :" $HHpercent
+HHHpercent=$( echo $HHH $number | awk '{print $1/$2*100}' )
+echo "Percent of Triplet combination(HHH) :" $HHHpercent
 
-TTpercent=$( echo $TT $number | awk '{print $1/$2*100}' )
-echo "Percent of Doublet combination(TT) :" $TTpercent
+HHTpercent=$( echo $HHT $number | awk '{print $1/$2*100}' )
+echo "Percent of Triplet combination(HHT) :" $HHTpercent
 
-THpercent=$( echo $TH $number | awk '{print $1/$2*100}' )
-echo "Percent of Doublet combination(TH) :" $THpercent
+HTHpercent=$( echo $HTH $number | awk '{print $1/$2*100}' )
+echo "Percent of Triplet combination(HTH) :" $HTHpercent
 
-HTpercent=$( echo $HT $number | awk '{print $1/$2*100}' )
-echo "Percent of Doublet combination(HT) :" $HTpercent
+HTTpercent=$( echo $HTT $number | awk '{print $1/$2*100}' )
+echo "Percent of Triplet combination(HTT) :" $HTTpercent
 
+THHpercent=$( echo $THH $number | awk '{print $1/$2*100}' )
+echo "Percent of Triplet combination(THH) :" $THHpercent
+
+THTpercent=$( echo $THT $number | awk '{print $1/$2*100}' )
+echo "Percent of Triplet combination(THT) :" $THTpercent
+
+TTHpercent=$( echo $TTH $number | awk '{print $1/$2*100}' )
+echo "Percent of Triplet combination(TTH) :" $TTHpercent
+
+TTTpercent=$( echo $TTT $number | awk '{print $1/$2*100}' )
+echo "Percent of Triplet combination(TTT) :" $TTTpercent
